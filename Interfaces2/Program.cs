5a0ffa721f6aa5c5ae80451cb1ff78c195ac4be3
Yağmur <yagmurkaranfil48@gmail.com>
2024-02-18 18:54:22 +0300

    Programci_serisi
    week2_2

---
+++ Interfaces2/Program.cs
@@ -0,0 +1,80 @@
+﻿namespace Interfaces2
+{
+    public class Program
+    {
+        //interfaceler newlenemez
+        static void Main(string[] args)
+        {
+           // IPersonManager customerManager = new CustomerManager();
+          
+           //IPersonManager employeeManager = new EmployeeManager();
+
+            ProjectManager projectManager = new ProjectManager();
+            projectManager.Add(new EmployeeManager());
+           
+        }
+    }
+
+    interface IPersonManager
+    {
+        //unimplemented operation
+        void Add();
+        void Update();
+    }
+
+    class CustomerManager : IPersonManager
+    {
+        public void Add()
+        {
+            //müşteri ekleme kodları
+            Console.WriteLine("Müşteri eklendi");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Müşteri güncellendi");
+
+        }
+    }
+
+    class EmployeeManager : IPersonManager
+    {
+        public void Add()
+        {
+            //personal ekleme kodları
+            Console.WriteLine("Personal eklendi");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Personal güncellendi");
+
+        }
+    }
+
+    class InternManager : IPersonManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Stajyer eklendi");
+
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Stajyer güncellendi");
+
+        }
+    }
+
+    class ProjectManager
+    {
+        public void Add(IPersonManager personManager)
+        {
+            personManager.Add();
+        }
+        
+      
+
+    }
+}
---
+++ ThreadingDemo1/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ThreadingDemo1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnProcess1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("İşlem1 çalıştı");
+        }
+
+        private void btnProcess2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("İşlem2 çalıştı");
+
+        }
+    }
+}
